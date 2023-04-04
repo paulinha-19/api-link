8a9c8b180763f1a5711e44b451127d2d26c24a37
Paulinha <paulafabianasoares@gmail.com>
2023-04-04 19:14:52 -0300

    db.js

--- api/config/db.js
+++ api/config/db.js
@@ -1,6 +1,5 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv/config.js";
-import dB from "./config";
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
