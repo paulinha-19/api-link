c726a705101ce9c511f199a4bfcfad2098affaf2
Paulinha <paulafabianasoares@gmail.com>
2023-04-04 20:49:15 -0300

    remoção do dialectOptions em db.js

--- api/config/db.js
+++ api/config/db.js
@@ -4,10 +4,7 @@ import dotenv from "dotenv/config.js";
 const db = new Sequelize(process.env.DB_NAME || "gerenciar-links", process.env.DB_USERNAME || "root", process.env.DB_PASSWORD || "sqlroot", {
     host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
-    port: process.env.DB_PORT || 3306,
-    dialectOptions: {
-        ssl: true
-    }
+    port: process.env.DB_PORT || 3306
 });
 
 export default db 
