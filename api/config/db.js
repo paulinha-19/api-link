7c172d1ae06a82ee38259fdce227bb242d0042b9
Paulinha <paulafabianasoares@gmail.com>
2023-04-04 20:43:48 -0300

    modificação no db.js

--- api/config/db.js
+++ api/config/db.js
@@ -1,10 +1,13 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv/config.js";
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const db = new Sequelize(process.env.DB_NAME || "gerenciar-links", process.env.DB_USERNAME || "root", process.env.DB_PASSWORD || "sqlroot", {
+    host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT || 3306,
+    dialectOptions: {
+        ssl: true
+    }
 });
 
 export default db 
