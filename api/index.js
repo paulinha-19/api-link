e4909a0f5a47bfa88ddf857c01c7d7635524c0fa
Paulinha <paulafabianasoares@gmail.com>
2023-09-18 16:41:21 -0300

    change db mysql-postgres

--- api/index.js
+++ api/index.js
@@ -7,13 +7,14 @@ const app = express();
 require("dotenv").config();
 
 (async () => {
-    try {
-        await db.sync();
-        console.log(`. O banco de dados ${process.env.DB_NAME} foi conectado`);
-    }
-    catch (error) {
-        console.error(`Erro ao conectar o banco de dados ${process.env.DB_NAME}. ${error} `);
-    }
+  try {
+    await db.sync();
+    console.log(`. O banco de dados ${process.env.DB_NAME} foi conectado`);
+  } catch (error) {
+    console.error(
+      `Erro ao conectar o banco de dados ${process.env.DB_NAME}. ${error} `
+    );
+  }
 })();
 
 app.use(express.json());
@@ -23,8 +24,5 @@ app.use(cors());
 app.use(linkRouter);
 
 app.listen(process.env.PORT || 3000, () =>
-    console.log(`Servidor iniciado na porta ${process.env.PORT}`)
+  console.log(`Servidor iniciado na porta ${process.env.PORT}`)
 );
-
-
-
